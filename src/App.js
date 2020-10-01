dcf3bf3af2a771aebb0d82384c87eefced5dc308
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2020-10-01 20:28:49 +0530

    Added Sign Up form

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+	
 export default class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div className='App'>
-					<nav className='navbar navbar-expand-lg navbar-light fixed-top'>
-						<div className='container'>
-							<Link className='navbar-brand' to={"/sign-in"}>
-								Template
+				<div className="App">
+					<nav className="navbar navbar-expand-lg navbar-light fixed-top">
+						<div className="container">
+							<Link className="navbar-brand" to={"/sign-in"}>
+								Login Template
 							</Link>
 							<div
-								className='collapse navbar-collapse'
-								id='login-menu'>
-								<ul className='navbar-nav ml-auto'>
-									<li className='nav-item'>
+								className="collapse navbar-collapse"
+								id="login-menu">
+								<ul className="navbar-nav ml-auto">
+									<li className="nav-item">
 										<Link
-											className='nav-link'
+											className="nav-link"
 											to={"/sign-in"}>
 											Login
+										</Link>
+									</li>
+									<li className="nav-item">
+										<Link
+											className="nav-link"
+											to={"/sign-up"}>
+											Signup
 										</Link>
 									</li>
 								</ul>
@@ -31,11 +39,12 @@ export default class App extends Component {
 						</div>
 					</nav>
 
-					<div className='auth-wrapper'>
-						<div className='auth-inner'>
+					<div className="auth-wrapper">
+						<div className="auth-inner">
 							<Switch>
-								<Route exact path='/' component={Login} />
-								<Route path='/sign-in' component={Login} />
+								<Route exact path="/" component={Login} />
+								<Route path="/sign-in" component={Login} />
+								<Route path="/sign-up" component={Signup} />
 							</Switch>
 						</div>
 					</div>
---
+++ src/components/Signup.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+export default class Signup extends Component {
+	render() {
+		return (
+			<form>
+				<h3>Sign Up</h3>
+
+				<div className='form-group'>
+					<label>First name</label>
+					<input
+						type='text'
+						className='form-control'
+						placeholder='First name'
+					/>
+				</div>
+
+				<div className='form-group'>
+					<label>Last name</label>
+					<input
+						type='text'
+						className='form-control'
+						placeholder='Last name'
+					/>
+				</div>
+
+				<div className='form-group'>
+					<label>Date of Birth</label>
+					<input
+						type='date'
+						className='form-control'
+					/>
+				</div>
+
+				<div className='form-group'>
+					<label>Email address</label>
+					<input
+						type='email'
+						className='form-control'
+						placeholder='Enter email'
+					/>
+				</div>
+
+				<div className='form-group'>
+					<label>Password</label>
+					<input
+						type='password'
+						className='form-control'
+						placeholder='Enter password'
+					/>
+				</div>
+
+				<div className='form-group'>
+					<label>Verify Password</label>
+					<input
+						type='password'
+						className='form-control'
+						placeholder='Verify password'
+					/>
+				</div>
+
+				<button type='submit' className='btn btn-primary btn-block'>
+					Sign Up
+				</button>
+				<p className='forgot-password'>
+					Already registered? <a href='/sign-in'> Sign In </a>
+				</p>
+			</form>
+		);
+	}
+}
