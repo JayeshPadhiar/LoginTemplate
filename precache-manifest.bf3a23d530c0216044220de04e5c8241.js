257350daa417904817a4a29a63093fceaecf8314
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2020-12-09 22:40:43 +0530

    Updates

--- precache-manifest.bf3a23d530c0216044220de04e5c8241.js
+++ precache-manifest.bf3a23d530c0216044220de04e5c8241.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1af6345c4e380db0038a246d4228801f",
+    "revision": "2ae7072bc52ed6a64d3d6841b1e04638",
     "url": "/login-template/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/login-template/static/css/2.40e75aa4.chunk.css"
   },
   {
-    "revision": "f376bd0edcab62bf009f",
+    "revision": "9d6e2336ef14bac8f208",
     "url": "/login-template/static/css/main.57200757.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/login-template/static/js/2.4160a5b1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f376bd0edcab62bf009f",
-    "url": "/login-template/static/js/main.9fde9673.chunk.js"
+    "revision": "9d6e2336ef14bac8f208",
+    "url": "/login-template/static/js/main.742ad003.chunk.js"
   },
   {
     "revision": "dfed804377dd1744fb91",
